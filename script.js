1e937347f6585f19b5a0b93b6e15dedfa1c6c13c
Gustavo Pöpper <96191613+poppergustavo@users.noreply.github.com>
2022-07-15 11:59:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+console.log("Bom dia Sr. Gustavo.");
+
+        var c = document.getElementById("c");
+        var ctx = c.getContext("2d");
+
+        //full screen
+        c.height = window.innerHeight;
+        c.width = window.innerWidth;
+
+        //chinese characters
+        var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
+
+        // abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}
+
+        //converting the string into an array of single characters
+        matrix = matrix.split("");
+
+        var font_size = 4;
+        var columns = c.width/font_size; //number of columns for the rain
+        //an array of drops - one per column
+        var drops = [];
+        //x below is the x coordinate
+        //1 = y co-ordinate of the drop(same for every drop initially)
+        for(var x = 0; x < columns; x++)
+            drops[x] = 1; 
+
+        //draw
+        function draw()
+        {
+            //Black BG for the canvas
+            //translucent BG to show trail
+            ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+            ctx.fillRect(0, 0, c.width, c.height);
+
+            ctx.fillStyle = "#55FF00";//green text   55FF00
+            ctx.font = font_size + "px arial";
+            //looping over drops
+            for(var i = 1; i < drops.length; i++)
+            {
+                //a random chinese
+                var text = matrix[Math.floor(Math.random()*matrix.length)];
+                //x = i*font_size, y = value of drops[i]*font_size
+                ctx.fillText(text, i*font_size, drops[i]*font_size);
+
+                //sending the drop back to the top randomly after it has crossed the screen
+                //adding a randomness to the reset to make the drops scattered on the Y axis
+                if(drops[i]*font_size > c.height && Math.random() > 0.975)
+                    drops[i] = 0;
+
+                //incrementing Y coordinate
+                drops[i]++;
+            }
+        }
+
+        setInterval(draw, 13);
